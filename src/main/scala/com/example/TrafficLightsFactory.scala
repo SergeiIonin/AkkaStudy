e3259584834446c6dd112dd1381acd801137aae9
SergeiIonin <52527592+SergeiIonin@users.noreply.github.com>
2019-07-20 15:37:57 +0300

    Update TrafficLightsFactory.scala

--- src/main/scala/com/example/TrafficLightsFactory.scala
+++ src/main/scala/com/example/TrafficLightsFactory.scala
@@ -1,13 +1,13 @@
 package com.example
 
 import akka.actor.{ActorRef, ActorSystem, Props}
-import com.example.TrafficLightsExample.{GreenLight, RedLight, YellowLight}
+import com.example.TrafficLightsExample.{TrafficLightActor}
 
 class TrafficLightsFactory(RedLightActor: ActorRef, YellowLightActor: ActorRef, GreenLightActor: ActorRef, system: ActorSystem)
 
 object TrafficLightsFactory {
   val system = ActorSystem("TrafficLights")
-  val RedLightActor: ActorRef = system.actorOf(Props(RedLight), "redLight")
-  val YellowLightActor: ActorRef = system.actorOf(Props(YellowLight), "yellowLight")
-  val GreenLightActor: ActorRef = system.actorOf(Props(GreenLight), "greenLight")
+  val RedLightActor: ActorRef = system.actorOf(Props(TrafficLightActor(4000, "Red")), "redLight")
+  val YellowLightActor: ActorRef = system.actorOf(Props(TrafficLightActor(1000, "Yellow")), "yellowLight")
+  val GreenLightActor: ActorRef = system.actorOf(Props(TrafficLightActor(3000, "Green")), "greenLight")
 }
